b33b4d817c5cbd3c1a968f98faf56441b6d36625
AmilcarArmmand <armmand@gmail.com>
2021-05-03 03:14:35 +0000

    updated __init__ in basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+import models
 
 
 Base = declarative_base()
@@ -19,50 +20,38 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if len(kwargs) == 0:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            for key, val in kwargs.items():
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
                 """Convert from str to object"""
                 if key == 'updated_at' or key == 'created_at':
-                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, val)
-                elif key != '__class__':
-                    setattr(self, key, val)
-            if 'id' not in kwargs or self.id is None:
-                self.id = str(uuid.uuid4())
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        # try:
-        from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
-        # except:
-        #     print("base model save does not work")
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary:
-            dictionary.pop('_sa_instance_state')
-        return dictionary
+        new_dict = dict(self.__dict__)
+        new_dict['__class__'] = str(type(self).__name__)
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in new_dict.keys():
+            del new_dict['_sa_instance_state']
+        return new_dict
 
     def delete(self):
         """Delete"""
-        from models import storage
-        storage.delete(self)
+        models.storage.delete(self)
